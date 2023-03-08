88aae3ff245f4d0949a4c9787ea6a9084f61d208
Mỹ Nguyễn <mynguyen@Mys-MacBook-Pro.local>
2023-03-08 09:30:15 -0800

    Episode 4.9

--- app/src/main/java/com/theandroidfactory/senya/DetailFragment.kt
+++ app/src/main/java/com/theandroidfactory/senya/DetailFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
 import com.theandroidfactory.senya.databinding.FragmentDetailBinding
 
-class DetailFragment: Fragment() {
+class DetailFragment: BaseFragment() {
     lateinit var binding: FragmentDetailBinding
     private val safeArgs: DetailFragmentArgs by navArgs()
+    private val attraction: Attraction by lazy {
+        attractions.find { it.id == safeArgs.id }!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +27,14 @@ class DetailFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.text = safeArgs.id
+
+        binding.title.text = attraction.title
+        binding.description.text = attraction.description
+        binding.monthsToVisit.text = attraction.months_to_visit
+        Picasso.get().load(attraction.image_urls[0]).into(binding.header)
+        binding.facts.text = "${attraction.facts.size} facts"
+        binding.facts.setOnClickListener {
+
+        }
     }
 }
